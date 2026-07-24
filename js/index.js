cffdfea991728e4c40ebbb58e8e4749ba2690239
Postigic <wenkaiye13@gmail.com>
2026-07-24 16:03:38 +0800

    even more setup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 import { observeElements } from "./common.js";
 
 observeElements({ elements: document.querySelector("h1") });
+observeElements({ elements: document.querySelector("#description") });
