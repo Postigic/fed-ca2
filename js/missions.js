bfadfa53a2f1edd18ae5e892ad7aa5a205df50ab
Postigic <wenkaiye13@gmail.com>
2026-07-28 05:17:16 +0800

    move all html to root, content and consistency additions to missions.html, and accessibility improvements

--- js/missions.js
+++ js/missions.js
@@ -1,12 +1,22 @@
+import { observeElements } from "./common.js";
+
 const cards = document.querySelectorAll(".timeline-card");
 
 cards.forEach((card) => {
+    const button = card.querySelector("button");
+    const wrap = card.querySelector(".extra-info-wrap");
+    const icon = card.querySelector("span");
+
     card.addEventListener("click", () => {
-        const info = card.querySelector(".extra-info");
-        const icon = card.querySelector("span");
+        const isOpen = wrap.classList.toggle("is-open");
 
-        info.classList.toggle("hidden");
-
-        icon.textContent = info.classList.contains("hidden") ? "+" : "−";
+        icon.textContent = isOpen ? "−" : "+";
+        button.setAttribute("aria-expanded", String(isOpen));
     });
+});
+
+observeElements({
+    elements: document.querySelector("#timeline"),
+    desktopThreshold: 0.1,
+    mobileThreshold: 0.1,
 });
