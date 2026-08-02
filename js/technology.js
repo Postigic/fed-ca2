052311082c9a3db79b77862d3718485975479872
Postigic <wenkaiye13@gmail.com>
2026-08-02 22:32:26 +0800

    fix inconsistencies

--- js/technology.js
+++ js/technology.js
@@ -1,3 +1,8 @@
+import { observeElements } from "./common.js";
+
+observeElements({ elements: document.querySelector("#intro") });
+observeElements({ elements: document.querySelector("#technologies") });
+
 const cards = document.querySelectorAll(".tech-card");
 cards.forEach((card) => {
     card.addEventListener("click", () => {
