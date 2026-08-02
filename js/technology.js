8d1764c0bc9ba8317d747fcd4d53a6303a39b0b8
Reuven <reuvenlow2009@gmail.com>
2026-08-02 21:53:21 +0800

    Fix formatting issues

--- js/technology.js
+++ js/technology.js
@@ -1,6 +1,6 @@
 const cards = document.querySelectorAll(".tech-card");
-    cards.forEach((card) => {
-        card.addEventListener("click", () => {
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
         const info = card.querySelector(".extra-info");
         info.classList.toggle("hidden");
     });
